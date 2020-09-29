ffb1bbedc0bb405c8730c9751d29e3aecf256572
Mauricio Blum <mauriciocblum@gmail.com>
2020-09-29 11:47:00 +0100

    added homepage and some api fixes

--- pages/api/image/[...params].ts
+++ pages/api/image/[...params].ts
@@ -5,7 +5,8 @@ import axios from 'axios'
 
 const getRandomImageNumber = (max?: number): string => {
   // generate a random number between 1 and 10
-  const randomInt = Math.floor(Math.random() * max + 1 || 11)
+  const randomInt = Math.floor(Math.random() * max + 1 || Math.random() * 11)
+  console.log(randomInt)
   return randomInt.toString()
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,73 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import React, { useMemo, useEffect, useState } from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+import { NextPage } from 'next'
 
-export default function Home() {
-  const router = useRouter()
-  return <div />
+const Home: NextPage = () => {
+  const [websiteURL, setWebsiteURL] = useState(
+    'https://dummy-placeholders.vercel.app/'
+  )
+
+  useEffect(() => {
+    setWebsiteURL(window.location.href)
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Dummy Placeholders</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Grandstander:ital,wght@0,300;0,400;0,600;1,700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <main className={styles.main}>
+        <h1>Dummy placeholders</h1>
+        <h2>Yet another image placeholder generator.</h2>
+
+        <div className={styles.howto}>
+          <h3>How to use:</h3>
+          <p>
+            Replace search-term with anything and set your preferred width and
+            height. Height is optional.
+          </p>
+          <div className={styles.code}>
+            {`${websiteURL}api/image/{search-term}/{width}/{height}`}
+          </div>
+
+          <h3>Example:</h3>
+          <div className={styles.code}>
+            {`${websiteURL}api/image/food/100/100`}
+          </div>
+          <h3>Turns into:</h3>
+          <img
+            src={`${websiteURL}api/image/food/100/100`}
+            alt="Generated Dummy"
+          />
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <span>
+          Developed by{' '}
+          <a
+            href="https://twitter.com/maublum"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            @maublum
+          </a>{' '}
+          and powered by{' '}
+          <a
+            href="https://pixabay.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Pixabay
+          </a>
+        </span>
+      </footer>
+    </div>
+  )
 }
+
+export default Home
